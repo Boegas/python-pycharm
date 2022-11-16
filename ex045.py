31e497bcacdb1472899ce39028965f3097f512a6
Anderson Jose Torquato <andersonboegas@gmail.com>
2022-11-16 14:47:10 -0300

    teste pycharm

---
+++ ex045.py
@@ -0,0 +1,47 @@
+from random import randint
+from time import sleep
+itens = ('pedra', 'papel', 'tesoura')
+computador = randint(0, 2)
+print('''Suas opções:
+[ 0 ] PEDRA 
+[ 1 ] PAPEL
+[ 2 ] TESOURA''')
+jogador = int(input('Qual é a sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO!!!!')
+print('-=' * 14)
+print('O computador escolheu {}'.format(itens[computador]))
+print('O jogador jogou {}'.format(itens[jogador]))
+print('-=' * 14)
+if computador == 0: #COMPUTADOR JOGOU PEDRA
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print('JOGADOR VENCE')
+    elif jogador == 2:
+        print('COMPUTADOR VENCE')
+    else:
+        print('JOGADA INVÁLIDA!')
+elif computador == 1:  #COMPUTADOR JOGOU PAPEL
+    if jogador == 0:
+        print('COMPUTADOR VENCE')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('JOGADOR VENCE')
+    else:
+        print('JOGADA INVÁLIDA!')
+elif computador == 2:  #COMPUTADOR JOGOU TESOURA
+    if jogador == 0:
+        print('JOGADOR VENCE')
+    elif jogador == 1:
+        print('COMPUTADOR VENCE')
+    elif jogador == 2:
+        print('EMPATE')
+    else:
+        print('JOGADA INVÁLIDA!')
+
+
